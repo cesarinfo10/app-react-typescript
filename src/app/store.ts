35f98e938ba7dc6674df99bff93043e57730203f
César Sánchez <cesarinfo10@gmail.com>
2024-05-21 13:55:26 -0300

    crud0.1

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import personReducer from '../features/person/personeSlice'
+
+
+export default configureStore({
+  reducer: {
+    person: personReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    }),
+})
+
+// 29 Minutos
